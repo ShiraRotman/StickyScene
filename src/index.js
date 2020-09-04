54a320ed7223898a0492649da862eed50917c01a
ShiraRotman <shiranetmail@gmail.com>
2020-09-04 05:28:55 +0300

    Custom changes and initial page.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+
+function StickyScene(props)
+{
+	return (
+		<div className="w-100 h-100">
+			<img src={props.scene} className="w-100 h-100" alt="Background Scene"/>
+		</div>
+	);
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	<React.StrictMode>
+		<StickyScene scene={process.env.PUBLIC_URL + "/hardcoded/underwater-treasures.jpg"}/>
+	</React.StrictMode>,
+	document.getElementById("stickySceneRoot"));
