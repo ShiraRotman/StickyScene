63430f3ee1d55d354aea0fc9a21d95e07d425605
ShiraRotman <shiranetmail@gmail.com>
2020-09-04 23:09:19 +0300

    Adding an options menu.

--- src/index.js
+++ src/index.js
@@ -2,26 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Tippy from "@tippyjs/react";
 
+const OPTIONS_MENU_ICONS=["newscene","loadscene","savescene"];
+
 class FloatingMenus extends React.Component
 {
 	constructor(props)
 	{ 
 		super(props); this.state={ shown: false };
 		this.toggleMenus=this.toggleMenus.bind(this);
+		this.closeMenus=this.closeMenus.bind(this);
 	}
 	
 	toggleMenus() 
 	{ this.setState(state => ({ shown: !this.state.shown })); }
+	closeMenus() { this.setState({ shown: false }); }
 	
 	render()
 	{
+		let toggleClassName="menu-toggle icon-wrapper";
+		if (this.state.shown) toggleClassName+=" toggled";
+		else toggleClassName+=" untoggled";
+		
 		return (
 			<Tippy interactive={true} arrow={false} offset={[0,0]} visible={this.state.shown}
-				placement="bottom-start" content={<span>Menu Item</span>}>
-				<button type="button" className="menu-toggle" onClick={this.toggleMenus}>
-					<img src={process.env.PUBLIC_URL + "/hamburger.svg"} className="w-100 h-100"
-						alt="3 horizontal lines"/>
-				</button>
+				placement="bottom-start" theme="light" className="floating-menu"
+				onClickOutside={this.closeMenus} content={OPTIONS_MENU_ICONS.map(icon => 
+				<div className="menu-option icon-wrapper my-1" key={icon.substring(0,icon.indexOf("scene"))}>
+					<img src={process.env.PUBLIC_URL + "icons/" + icon + ".svg"} alt="Menu Option"
+						className="w-100 h-100"/>
+				</div>)}>
+				<div className={toggleClassName} onClick={this.toggleMenus}>
+					<img src={process.env.PUBLIC_URL + "icons/menus.svg"} className="w-100 h-100"
+						alt="Menu Button"/>
+				</div>
 			</Tippy>
 		);
 	}
