2e9ffbb01353b7d9485893c75251e5ce0467257c
ShiraRotman <shiranetmail@gmail.com>
2020-09-13 23:00:36 +0300

    Updates

---
+++ precache-manifest.9e5e829b8641cd1577220d8e0f8e318b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa134960daedfad2d9e74b1cd05ebe7",
+    "url": "/StickyScene/index.html"
+  },
+  {
+    "revision": "b7d09a82763097602327",
+    "url": "/StickyScene/static/js/2.6be3c0bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/StickyScene/static/js/2.6be3c0bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b22486f0ab9edcf40bd2",
+    "url": "/StickyScene/static/js/main.cac05bd6.chunk.js"
+  },
+  {
+    "revision": "3124004a0d250130714b",
+    "url": "/StickyScene/static/js/runtime-main.c920bcfe.js"
+  }
+]);
